e83dcf8933e70cb3804c43c92b1a1d9ff88dcd0a
Gabriel Florit <gabrielflorit@gmail.com>
2014-03-21 19:49:01 -0400

    wired up most basic of pagination

---
+++ README.md
@@ -0,0 +1,7 @@
+# 04-01-2014_whyIRun
+
+## TODO
+
+- load first 10 stories
+- when "load more" is clicked, load next
+- create layer that takes care of pagination
--- js/globe.graphic.js
+++ js/globe.graphic.js
@@ -1,5 +1,25 @@
 globe.graphic = function() {
 
-	// code goes here
+	var page = 1;
+	var master = $('#gf');
+
+	function loadMoreStories() {
+
+		$.getJSON('http://private.boston.com/newsprojects/whyirun/get_stories_api.php?year=2013&page=' + page++, function(json) {
+
+			var html = [];
+			for (var i = 0; i < json.length; i++) {
+				html.push(window.JST['story.template'](json[i]));
+			}
+
+			$('.stories', master).append(html.join(''));
+
+		});
+
+	}
+
+	loadMoreStories();
+
+	$('button.loadMoreStories', master).click(loadMoreStories);
 
 };
