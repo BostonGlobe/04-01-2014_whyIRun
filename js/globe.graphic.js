543c7c02dfbc8a85516066d41f348242a7b2fedf
Gabriel Florit <gabrielflorit@gmail.com>
2014-04-01 17:46:47 -0400

    started rewriting the form as modal on mobile

--- js/globe.graphic.js
+++ js/globe.graphic.js
@@ -1,49 +1,53 @@
 globe.graphic = function() {
 
+	if (Modernizr.touch) {
+		$('.gf-form-wrapper', master).appendTo('body');
+	}
+
 	// from http://api.jquery.com/jQuery.cssHooks/
 	(function( $ ) {
-	 
-	if ( !$.cssHooks ) {
-	  throw( new Error( "jQuery 1.4.3+ is needed for this plugin to work" ) );
-	}
-	 
-	function styleSupport( prop ) {
-	  var vendorProp, supportedProp,
-	    capProp = prop.charAt( 0 ).toUpperCase() + prop.slice( 1 ),
-	    prefixes = [ "Moz", "Webkit", "O", "ms" ],
-	    div = document.createElement( "div" );
-	 
-	  if ( prop in div.style ) {
-	    supportedProp = prop;
-	  } else {
-	    for ( var i = 0; i < prefixes.length; i++ ) {
-	      vendorProp = prefixes[ i ] + capProp;
-	      if ( vendorProp in div.style ) {
-	        supportedProp = vendorProp;
-	        break;
-	      }
-	    }
-	  }
-	 
-	  div = null;
-	  $.support[ prop ] = supportedProp;
-	  return supportedProp;
-	}
-	 
-	var transform = styleSupport( "transform" );
-	 
-	// Set cssHooks only for browsers that support a vendor-prefixed border radius
-	if ( transform && transform !== "transform" ) {
-	  $.cssHooks.transform = {
-	    get: function( elem, computed, extra ) {
-	      return $.css( elem, transform );
-	    },
-	    set: function( elem, value) {
-	      elem.style[ transform ] = value;
-	    }
-	  };
-	}
-	 
+
+		if ( !$.cssHooks ) {
+			throw( new Error( "jQuery 1.4.3+ is needed for this plugin to work" ) );
+		}
+
+		function styleSupport( prop ) {
+			var vendorProp, supportedProp,
+				capProp = prop.charAt( 0 ).toUpperCase() + prop.slice( 1 ),
+				prefixes = [ "Moz", "Webkit", "O", "ms" ],
+				div = document.createElement( "div" );
+
+			if ( prop in div.style ) {
+				supportedProp = prop;
+			} else {
+				for ( var i = 0; i < prefixes.length; i++ ) {
+					vendorProp = prefixes[ i ] + capProp;
+					if ( vendorProp in div.style ) {
+						supportedProp = vendorProp;
+						break;
+					}
+				}
+			}
+
+			div = null;
+			$.support[ prop ] = supportedProp;
+			return supportedProp;
+		}
+
+		var transform = styleSupport( "transform" );
+
+		// Set cssHooks only for browsers that support a vendor-prefixed border radius
+		if ( transform && transform !== "transform" ) {
+			$.cssHooks.transform = {
+				get: function( elem, computed, extra ) {
+					return $.css( elem, transform );
+				},
+				set: function( elem, value) {
+					elem.style[ transform ] = value;
+				}
+			};
+		}
+
 	})( jQuery );
 
 	var page = 1;
@@ -88,15 +92,48 @@ globe.graphic = function() {
 
 	$('button.why', master).click(function(e) {
 
-		slideStoriesDown();
-		$('button.why', master).parent().find('.why,.title').toggleClass('hidden');
+		function draw() {
+			slideStoriesDown();
+			$('button.why', master).parent().find('.why,.title').toggleClass('hidden');
+		}
+
+		if (Modernizr.touch) {
+
+			// var parent = $('button.why', master).parent();
+			// parent.removeClass('sticky');
+
+			// $('html, body').animate({
+			// scrollTop: parent.offset().top
+			// }, 0).promise().done(draw);
+
+		} else {
+			draw();
+		}
 
 	});
 
 	$('button.cancel, button.title', master).click(function(e) {
 
-		slideStoriesUp();
-		$('button.why', master).parent().find('.why,.title').toggleClass('hidden');
+		function draw() {
+			slideStoriesUp();
+			$('button.why', master).parent().find('.why,.title').toggleClass('hidden');
+		}
+
+		if (Modernizr.touch) {
+
+			// var parent = $('button.why', master).parent();
+
+			// $('html, body').animate({
+			// scrollTop: parent.offset().top
+			// }, 0).promise().done(function() {
+
+			// parent.addClass('sticky');
+			// draw();
+			// });
+
+		} else {
+			draw();
+		}
 
 	});
 
@@ -104,23 +141,28 @@ globe.graphic = function() {
 
 	$('button.submit', master).click(function(e) {
 
-		slideStoriesUp();
-		$('button.why', master).parent().find('.why,.title').addClass('hidden');
-		$('button.why', master).parent().find('.thanks').removeClass('hidden');
+		function draw() {
+			slideStoriesUp();
+			$('button.why', master).parent().find('.why,.title').addClass('hidden');
+			$('button.why', master).parent().find('.thanks').removeClass('hidden');
+		}
+
+		if (Modernizr.touch) {
+
+			// var parent = $('button.why', master).parent();
+
+			// $('html, body').animate({
+			// scrollTop: parent.offset().top
+			// }, 0).promise().done(function() {
+
+			// parent.removeClass('sticky');
+			// draw();
+			// });
+
+		} else {
+			draw();
+		}
 
 	});
 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
