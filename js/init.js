3f0a92505753f671412e2429939ad6f5ce3fbe23
Gabriel Florit <gabrielflorit@gmail.com>
2014-04-07 14:26:09 -0400

    added logging

--- js/init.js
+++ js/init.js
@@ -1,3 +1,28 @@
 globe.onDefine('window.jQuery && $("#gf").length', function() {
 	globe.graphic();
 });
+if (window.console && console.log) {
+	console.log('      ########                ##        ');
+	console.log('     ##########              ####       ');
+	console.log('  ################       #  #######     ');
+	console.log(' #            ##       #      ######    ');
+	console.log('##           #       #        #######   ');
+	console.log('           ###    ####          ####### ');
+	console.log('         #####   #####          ########');
+	console.log('        ####    ######            ####  ');
+	console.log('        ####    ######            ##    ');
+	console.log('        ####    ######        ####      ');
+	console.log('############    ######      ########    ');
+	console.log('##    ######    ######      ############');
+	console.log('        ####    ######          ########');
+	console.log('        ####    ####            ########');
+	console.log('      #####     #               ########');
+	console.log('         #    #                 ########');
+	console.log('          ####                  ########');
+	console.log('    ##################          ####### ');
+	console.log('  ###          #########        #       ');
+	console.log('  #                #########  #         ');
+	console.log('##                      ###             ');
+	console.log('  ##');
+	console.log('Source code for this project at https://github.com/BostonGlobe/04-01-2014_whyIRun .');
+}
